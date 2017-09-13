b3994a248da8cec6a75c36bcfe262bb0f9e2efa5
Aveek <nsaveek@gmail.com>
2017-09-14 00:23:39 +0800

    Ui not fixed

--- app/src/main/java/uitemplates/aveek/com/MainActivity.java
+++ app/src/main/java/uitemplates/aveek/com/MainActivity.java
@@ -4,13 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.facebook.FacebookSdk;
+import com.facebook.LoggingBehavior;
+import com.facebook.appevents.AppEventsLogger;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getApplicationContext());
+//        FacebookSdk.setApplicationId(APP_ID);
+//        FacebookSdk.sdkInitialize(getApplicationContext());
+//        AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_main);
     }
 }
